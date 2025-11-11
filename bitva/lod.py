fa8fc15d31eb7f30d030a97a113a55181866034b
zcb00m <zane.cicha@gmail.com>
2025-11-11 15:05:35 +0100

    snad uz to pofachne

--- bitva/lod.py
+++ bitva/lod.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Lod a odvozene tridy pro vesmirny souboj.
+Lod a odvozene tridy pro souboj.
 '''
 
 class Lod:
@@ -17,9 +17,19 @@ class Lod:
         self._stit = stit
         self._kostka = kostka
         self._zprava = ''
-
+    
     def __str__(self):
         return str(self._jmeno)
+    
+    def je_operacni(self):
+        return self._trup > 0
+
+    def graficky_trup(self):
+        celkem = 20
+        pocet = int(self._trup / self._max_trup * celkem)
+        if pocet == 0 and self.je_operacni():
+            pocet = 1
+        return f"[{'#'*pocet}{' '*(celkem-pocet)}]"    
 
     def utoc(self, souper):
         uder = self._utok + self._kostka.hod()
@@ -35,9 +45,36 @@ class Lod:
             if self._trup < 0:
                 self._trup = 0
                 zprava = f'{zprava[:-1]} a byla znicena.'
+        else:
+            zprava = f'{self._jmeno} odrazil utok stity.'
+        self.nastav_zpravu(zprava)
 
     def nastav_zpravu(self, zprava):
         self._zprava = zprava
-
+    
     def vypis_zpravu(self):
         return self._zprava
+
+
+class Stihac(Lod):
+    """
+    Odvozena trida, ktera pridava energii pro laserovy vyboj.
+    Demonstruje dedicnost, polymorfismus a pretizeni metody.
+    """
+
+    def __init__(self, jmeno, trup, utok, stit, kostka, energie, laserovy_utok):
+        super().__init__(jmeno, trup, utok, stit, kostka)
+        self._energie = energie
+        self._max_energie = energie 
+        self._laserovy_utok = laserovy_utok
+
+    def utoc(self, souper):
+        if self._energie < self._max_energie:
+            self._energie = min(self._max_energie, self._energie + 10)
+            super().utoc(souper)
+        else:
+            uder = self._laserovy_utok + self._kostka.hod()
+            self.nastav_zpravu(f'{self._jmeno} utoci Laserem o sile {uder} hp.')
+            self._energie = 0
+            souper.bran_se(uder)
+
--- bitva/main.py
+++ bitva/main.py
@@ -1,41 +1,83 @@
 #!/usr/bin/env python3
 
 from kostka import Kostka 
-from lod import Lod 
+from lod import Lod, Stihac
 
 class Sektor:
     """
     Správa souboje dvou lodí
     """
 
-    def __init__(self, lod_1, lod_2, kostka):
+    def __init__(self, lod_1, lod_2, kostka, jmeno="bez nazvu",):
+        self._jmeno = jmeno
         self._lod_1 = lod_1
         self._lod_2 = lod_2
         self._kostka = kostka
 
+    def _vycisti(self):
+        import sys as _sys
+        import subprocess as _subprocess
+        if _sys.platform.startswith('win'):
+            _subprocess.call(['cmd.exe', '/C', 'cls'])
+        else:
+            _subprocess.call(['clear'])
+        
+    def _vypis_lod(self, lod):
+        print(lod)
+        print(f'Trup: {lod.graficky_trup()}')
+
+    def _vykresli(self):
+        self._vycisti()
+        print(f'================ Sektor {self._jmeno} ================\n')
+        print('Lodě:\n')
+        self._vypis_lod(self._lod_1)
+        self._vypis_lod(self._lod_2)
+        print()
+
+
     def souboj(self):
-        print("Vitej v randomackem sektoru!")
+        print(f"Vitej v sektoru {self._jmeno}!")
         print("======================")
         print()
-        print(f"Dnes se utkaji lode {self._lod_1} a {self._lod_2}.")
+        print(f"Dnes se utkaji lode:")
+        self._vypis_lod(self._lod_1)
+        self._vypis_lod(self._lod_2)
         print("Zahajit souboj...")
         input()
 
-        self._lod_1.utoc(self._lod_2)
-        self._vypis_zpravu(self._lod_1.vypis_zpravu())
-        self._vypis_zpravu(self._lod_2.vypis_zpravu())
-        self._lod_2.utoc(self._lod_1)
-        self._vypis_zpravu(self._lod_2.vypis_zpravu())
-        self._vypis_zpravu(self._lod_1.vypis_zpravu())
+        import random
+        if random.randint(0, 1):
+            self._lod_1, self._lod_2 = self._lod_2, self._lod_1
+
+        while self._lod_1.je_operacni() and self._lod_2.je_operacni():
+            self._lod_1.utoc(self._lod_2)
+            self._vykresli()
+            self._vypis_zpravu(self._lod_1.vypis_zpravu())
+            self._vypis_zpravu(self._lod_2.vypis_zpravu())
+
+            if self._lod_2.je_operacni():
+                self._lod_2.utoc(self._lod_1)
+                self._vykresli()
+                self._vypis_zpravu(self._lod_2.vypis_zpravu())
+                self._vypis_zpravu(self._lod_1.vypis_zpravu())
 
 
     def _vypis_zpravu(self, zprava):
-        print(zprava)
+        import time as _time
+        if zprava:
+            print(zprava)
+            _time.sleep(0.3)
+
 
 if __name__ == '__main__':
     k = Kostka(10)
-    lodicka = Lod("Randomacka lod", 100, 80, 50, k)
-    clun = Lod("C(l)unik", 40, 20, 30, k)
-    orion = Sektor(lodicka, clun, k)
+    lodicka = Lod("Lawď", 100, 80, 50, k)
+    clun = Lod("Č(l)uník", 140, 20, 30, k)
+    l = Lod("Jót", kostka=k, trup=80, utok=60, stit=70)
+    fighter = Stihac("Stíhač", 90, 50, 60, k, 30, 90)
+    
+    orion = Sektor(lodicka, fighter, k, "Orion")
+    gamma = Sektor(lodicka, l, k, "Gamma")
 
     orion.souboj()
+    gamma.souboj()
